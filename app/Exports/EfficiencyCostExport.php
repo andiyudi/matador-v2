104edf1c9184c6e4a93b27eb8ab77d6e90dfa1d9
AndiYudi Rahmat Rusli <AndiYudi@citra.co.id>
2024-06-05 13:05:24 +0700

    add more filter start and end month in recap efficiency cost export

--- app/Exports/EfficiencyCostExport.php
+++ app/Exports/EfficiencyCostExport.php
@@ -10,22 +10,48 @@ use Maatwebsite\Excel\Concerns\FromView;
 
 class EfficiencyCostExport implements FromView
 {
-    public function view():View
+    public function view(): View
     {
         $year = Request::input('year');
-        $months = [];
-        $monthsName = [];
-        $procurementData=[];
-        for ($i = 1; $i <= 12; $i++) {
-            $months[] = $i; // Menggunakan angka bulan
-            $monthsName[] = Carbon::create($year, $i)->translatedFormat('F');
+        $start_month = Request::input('start_month');
+        $end_month = Request::input('end_month');
+
+        // Membuat range bulan berdasarkan start month dan end month
+        $months = range($start_month, $end_month);
+
+        // Mendapatkan nama bulan dalam bahasa Indonesia menggunakan fungsi getMonthsName
+        $monthsName = $this->getMonthsName($months);
+
+        $procurementData = [];
+
+        // Iterasi melalui rentang bulan untuk mendapatkan data procurement
+        foreach ($months as $month) {
             // Filter procurement berdasarkan tahun dan bulan
-            $procurementData[$i] = Procurement::whereYear('receipt', $year)
-                                                ->whereMonth('receipt', $i)
-                                                ->where('status', '1')
-                                                ->selectRaw('SUM(user_estimate) as total_user_estimate, SUM(deal_nego) as total_deal_nego')
-                                                ->first();
+            $procurementData[$month] = Procurement::whereYear('receipt', $year)
+                ->whereMonth('receipt', $month)
+                ->where('status', '1')
+                ->selectRaw('SUM(user_estimate) as total_user_estimate, SUM(deal_nego) as total_deal_nego')
+                ->first();
         }
+
         return view('recapitulation.efficiency.result', compact('year', 'months', 'monthsName', 'procurementData'));
+    }
+
+    private function getMonthsArray()
+    {
+        return [
+            '01' => 'Januari', '02' => 'Februari', '03' => 'Maret', '04' => 'April', '05' => 'Mei', '06' => 'Juni',
+            '07' => 'Juli', '08' => 'Agustus', '09' => 'September', '10' => 'Oktober', '11' => 'November', '12' => 'Desember'
+        ];
+    }
+
+    private function getMonthsName($months)
+    {
+        $allMonths = $this->getMonthsArray(); // Ensure this function returns an array with month names indexed from '01' to '12'
+        $monthsName = [];
+        foreach ($months as $month) {
+            $monthsName[] = $allMonths[sprintf('%02d', $month)];
+        }
+        return $monthsName;
     }
 }
